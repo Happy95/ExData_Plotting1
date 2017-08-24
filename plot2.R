fb1901f9efc440712e6de4d8eaacd05488387d6a
Swastik Satpathy <30603033+Happy95@users.noreply.github.com>
2017-08-24 21:37:19 +0530

    Delete plot2.R

--- plot2.R
+++
@@ -1,9 +0,0 @@
-Electric <- read.csv("household_power_consumption.csv", na.strings = "?")
-Electric$Date <- as.Date(Electric$Date, "%d/%m/%Y")
-Power <- Electric[Electric$Date >= as.Date("2007-02-01") &
-                    Electric$Date <= as.Date("2007-02-02"), ]
-rm(Electric)
-DT <- paste(Power$Date, Power$Time)
-DT <- strptime(DT, "%Y-%m-%d %H:%M:%S")
-plot(DT, Power$Global_active_power, type = "l",
-     ylab = "Global Active Power(kilowatts)", xlab = " ")
