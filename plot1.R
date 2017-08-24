8f8086a6dfcc032b45dc44432d5b219ba1b4eb1d
Swastik Satpathy <30603033+Happy95@users.noreply.github.com>
2017-08-24 21:36:48 +0530

    Delete plot1.R

--- plot1.R
+++
@@ -1,7 +0,0 @@
-Electric <- read.csv("household_power_consumption.csv", na.strings = "?")
-Electric$Date <- as.Date(Electric$Date, "%d/%m/%Y")
-Power <- Electric[Electric$Date >= as.Date("2007-02-01") &
-                    Electric$Date <= as.Date("2007-02-02"), ]
-rm(Electric)
-hist(Power$Global_active_power, col = "red",
-     xlab = "Global Active Power(kilowatts)", main = "Global Active Power")
